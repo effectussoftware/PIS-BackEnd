27f1dd37d8526793b7bc05c28a8e7537b415bdfe
Juan Baz <juan@nayya.com>
2021-09-07 00:51:52 -0300

    add person model tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -24,5 +24,60 @@ RSpec.describe Person, type: :model do
     it 'test that factory is valid' do
       expect(person).to be_valid
     end
+
+    context 'when fields are invalid' do
+      it 'has invalid first_name' do
+        person.first_name = ''
+        expect(person).not_to be_valid
+      end
+
+      it 'has invalid first_name' do
+        person.last_name = ''
+        expect(person).not_to be_valid
+      end
+
+      it 'has invalid first_name' do
+        person.email = ''
+        expect(person).not_to be_valid
+      end
+
+      it 'has invalid weekely' do
+        person1 = build(:person,
+                        hourly_load: Person.hourly_load_types[:weekely],
+                        hourly_load_hours: 19)
+        expect(person1).not_to be_valid
+        person1.hourly_load_hours = 46
+        expect(person1).not_to be_valid
+      end
+
+      it 'has invalid daily' do
+        person1 = build(:person,
+                        hourly_load: Person.hourly_load_types[:daily],
+                        hourly_load_hours: 3)
+        expect(person1).not_to be_valid
+        person1.hourly_load_hours = 10
+        expect(person1).not_to be_valid
+      end
+    end
+
+    context 'when fields are valid' do
+      it 'has valid weekely' do
+        person1 = build(:person,
+                        hourly_load: Person.hourly_load_types[:weekely],
+                        hourly_load_hours: 20)
+        expect(person1).to be_valid
+        person1.hourly_load_hours = 45
+        expect(person1).to be_valid
+      end
+
+      it 'has valid daily' do
+        person1 = build(:person,
+                        hourly_load: Person.hourly_load_types[:daily],
+                        hourly_load_hours: 4)
+        expect(person1).to be_valid
+        person1.hourly_load_hours = 9
+        expect(person1).to be_valid
+      end
+    end
   end
 end
