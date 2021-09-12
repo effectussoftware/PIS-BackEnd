1f508594cb634521bb7ac2ea2a60874e3012b298
Juan Baz <juan@nayya.com>
2021-09-12 14:02:48 -0300

    add people controller actions

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,13 +1,22 @@
 module Api
   module V1
     class PeopleController < Api::V1::ApiController
-      def index; end
+      def index
+        # Render persons list
+      end
 
       def show; end
 
       def update; end
 
       def destroy; end
+
+      private
+
+      def person_params
+        params.require(:person).permit(:first_name, :last_name, :email, :hourly_load,
+                                       :hourly_load_hours)
+      end
     end
   end
 end
