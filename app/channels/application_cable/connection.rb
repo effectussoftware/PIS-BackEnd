5b939957d0ff6c8afa0e70ae720439a2ada48345
fcocasa <franciscocasarotti1994@gmail.com>
2021-10-07 16:49:56 -0300

    Se comunica correctamente con el front -- crea el websocket -- no esta las pruebas unitarias

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,25 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      client = request.params[:client]
+      uid = request.params[:uid]
+      token = request.params[:token]
+
+      self.current_user = find_verified_user token, uid, client
+    end
+
+    private
+
+    def find_verified_user(token, uid, client_id)
+      user = User.find_by email: uid
+      # http://www.rubydoc.info/gems/devise_token_auth/0.1.38/DeviseTokenAuth%2FConcerns%2FUser:valid_token%3F
+      if user && user.valid_token?(token, client_id)
+        user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
