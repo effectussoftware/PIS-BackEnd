1280474a4b0607409d837d80280a7a8f432f3c27
fcocasa <franciscocasarotti1994@gmail.com>
2021-10-07 16:49:56 -0300

    Se comunica correctamente con el front -- crea el websocket -- no esta las pruebas unitarias

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.url = 'ws://localhost:3000/cable'
+  config.action_cable.allowed_request_origins = [ /http:\/\/localhost:*/ ]
+  config.action_cable.disable_request_forgery_protection = true
   config.cache_classes = false
 
   # Do not eager load code on boot.
