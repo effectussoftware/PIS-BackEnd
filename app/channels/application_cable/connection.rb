fefdbb58dbcdd84edcd0aaf9a2197d9db99a410f
fcocasa <franciscocasarotti1994@gmail.com>
2021-10-08 19:57:21 -0300

    Agregando streaming para usuarios en particualr, para el caso de los crontab

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,13 +10,17 @@ module ApplicationCable
       self.current_user = find_verified_user token, uid, client
     end
 
+    def send_message
+      send_async
+    end
+
     private
 
     def find_verified_user(token, uid, client_id)
       user = User.find_by email: uid
       # http://www.rubydoc.info/gems/devise_token_auth/0.1.38/DeviseTokenAuth%2FConcerns%2FUser:valid_token%3F
       if user && user.valid_token?(token, client_id)
-        user
+        user.uid
       else
         reject_unauthorized_connection
       end
--- app/channels/web_channel.rb
+++ app/channels/web_channel.rb
@@ -1,9 +1,23 @@
 class WebChannel < ApplicationCable::Channel
+
   def subscribed
     stream_from "web_channel"
+    stream_for current_user
   end
 
   def unsubscribed
     stop_all_streams
   end
+
+  def self.send_message(uid)
+
+    self.broadcast_to(
+      User.find_by(email: uid),
+      data: 'A project is coming to an end'
+    )
+  end
+
 end
+
+
+
