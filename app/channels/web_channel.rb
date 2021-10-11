555f9e887fe7ca98a137ea50fbc86f51973f7dff
fcocasa <franciscocasarotti1994@gmail.com>
2021-10-11 19:01:41 -0300

    Refactorizacion observer -- avance alertas de proyecto

--- app/channels/web_channel.rb
+++ app/channels/web_channel.rb
@@ -16,6 +16,9 @@ class WebChannel < ApplicationCable::Channel
     stream_for current_user
   end
 
+    # Al conectarme verifico no tener notificaciones pendientes
+    # puts "Por enviar mensaje desde connection"
+    # current_user.check_alerts
   def unsubscribed
     stop_all_streams
   end
