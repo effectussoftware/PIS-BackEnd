93c49480de6957446a372dbfd1e24d88396990f4
Juancho Baz <juan.baz.pera@gmail.com>
2021-10-21 00:21:56 -0300

    add translations missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   after_action :verify_policy_scoped,
                only: :index,
                unless: -> { :devise_controller? || :active_admin_controller? }
-  before_action :set_locale
   # Prevent CSRF attacks by raising an exception.
   # protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: :json_request?
@@ -16,20 +15,5 @@ class ApplicationController < ActionController::Base
 
   def active_admin_controller?
     is_a?(ActiveAdmin::BaseController)
-  end
-
-  private
-
-  def set_locale
-    I18n.locale = extract_locale || I18n.default_locale
-  end
-
-  def extract_locale
-    parsed_locale = request.headers['locale']
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
   end
 end
