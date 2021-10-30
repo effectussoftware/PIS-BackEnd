94bf7321a61168678037fea375c7fc4308000688
Juancho Baz <45953832+juanbazpera@users.noreply.github.com>
2021-10-30 01:59:14 -0300

    list and edit admins (#31)
    * Se agrega project type hibrido
    
    * Se agrega listado de Usuarios
    
    * Agregado delete_user
    
    * Test delete_user OK
    
    * Update gemfile.lock
    
    * Arreglo delete user
    
    * Gemfile.lock windows
    
    * Gemfile.lock linux
    
    * update gemfile.lock
    
    Co-authored-by: Santiago Puppo Caubarrere <santiago.puppo@fing.edu.uy>
    Co-authored-by: Nicolas Adrién <nadrien@outlook.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class UsersController < Api::V1::ApiController
-      before_action :auth_user
-
       def show; end
 
       def update
@@ -10,10 +8,27 @@ module Api
         render :show
       end
 
+      def index
+        @admins = User.all
+        render :index
+      end
+
+      def destroy
+        user = User.find(params[:id])
+        if current_user?(user)
+          render json: { message: I18n.t('api.errors.user.invalid_delete') }
+        else
+          full_name = user.full_name
+          user.destroy!
+          render json: { message: I18n.t('api.success.user.record_delete',
+                                         { name: full_name }) }
+        end
+      end
+
       private
 
-      def auth_user
-        authorize current_user
+      def current_user?(user)
+        current_user.id == user.id
       end
 
       def user_params
---
+++ spec/requests/api/v1/users/delete_spec.rb
@@ -0,0 +1,17 @@
+describe 'GET api/v1/users/', type: :request do
+  # Needed to get auth_headers
+  let!(:user) { create(:user) }
+
+  subject do
+    delete api_v1_users_path(user.id), headers: auth_headers, as: :json
+  end
+
+  it 'should return success' do
+    subject
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'should delete the user on database' do
+    expect { subject }.to change { User.count }.by(-1)
+  end
+end
