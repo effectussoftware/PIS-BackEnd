d6c0964f1c704fa73e2d9c674e7f977ee13c4467
nicolasadrien <89556950+nicolasadrien@users.noreply.github.com>
2021-11-02 14:11:29 -0300

    Fix modificar fecha nula de proyecto impacte en asignaciones (#36)
    * Fix modificar fecha nula de proyecto impacte en asignaciones
    
    * Rubocop

--- app/models/project.rb
+++ app/models/project.rb
@@ -39,20 +39,28 @@ class Project < ApplicationRecord
   validate :budget_is_valid
   validate :end_date_is_after_start_date
 
-  after_update :update_person_projects
+  after_update :update_person_projects_date
+  after_update :update_person_projects_date_null
 
-  def update_person_projects
+  def update_person_projects_date
     person_projects = PersonProject.where('project_id = :project_id
-        AND start_date < :start_date', { project_id: id, start_date: start_date })
-    update_person_project_date(person_projects, :start_date, start_date)
+      AND start_date < :start_date', { project_id: id, start_date: start_date })
+    update_person_projects(person_projects, :start_date, start_date)
 
     person_projects = PersonProject.where('project_id = :project_id
       AND end_date > :end_date', { project_id: id, end_date: end_date })
-
-    update_person_project_date(person_projects, :end_date, end_date)
+    update_person_projects(person_projects, :end_date, end_date)
   end
 
-  def update_person_project_date(person_projects, date, update)
+  def update_person_projects_date_null
+    return if end_date.blank?
+
+    person_projects = PersonProject.where('project_id = :project_id
+      AND end_date is null', { project_id: id, end_date: end_date })
+    update_person_projects(person_projects, :end_date, end_date)
+  end
+
+  def update_person_projects(person_projects, date, update)
     person_projects.each do |p_p|
       p_p[date] = update
       p_p.save!
