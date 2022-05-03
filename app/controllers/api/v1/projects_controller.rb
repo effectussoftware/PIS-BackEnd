c5e9b909babf8d9cce8e14b9133f4bfa9cd70675
julietacaffera <jcaffera@effectussoftware.com>
2022-05-03 10:38:35 -0300

    projects can be filtered by active

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -12,6 +12,9 @@ module Api
       def index
         @projects = filter!(Project).includes(:person_project, :people, :project_technologies,
                                               :technologies, :notes)
+        if params[:active_project].present? && current_user.is_a?(User)
+          @projects = @projects.only_active
+        end
       end
 
       def show
